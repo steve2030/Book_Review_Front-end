579d2e0eac19ab7405108cb46712f66d5d3bfb92
steve <stevemighty2030@gmail.com>
2022-11-08 15:46:38 +0300

    resolved navbar conflicts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import Home from './components/Home';
+import BookDetails from './components/BookDetails';
+import Review from './components/Review';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Navbar/>
+     <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route exact path='/BookList' element={<BookList />} />
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
