2801c56f69d254fa8d5f0a30f6cfab48adf08b51
+ <+>
2022-11-08 21:57:34 +0300

    booklist cards

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import data from '../data'
+import Card from './Card'
+import BookListHero from './BookListHero'
 
 const BookList = () => {
-  return (
-    <div>BookList</div>
-  )
+        const cards = data.map(item => {
+            return (
+                <Card 
+                    key={item.id}
+                    item = {item}
+                />
+            )
+        })
+    
+        return (
+            <div>
+                <BookListHero />
+                <div className='cards-list'>
+                    {cards}
+                </div>
+            </div>
+        );
 }
 
 export default BookList
