48aded76226f6d3b24774496e831287264647226
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-11-08 20:27:04 +0300

    create booklist section

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import data from '../data'
+import Card from './Card'
+import BookListHero from './BookListHero'
 
 const BookList = () => {
-  
+        const cards = data.map(item => {
+            return (
+                <Card 
+                    key={item.id}
+                    item = {item}
+                />
+            )
+        })
+    
+        return (
+            <div>
+                <BookListHero />
+                <div className='cards-list'>
+                    {cards}
+                </div>
+            </div>
+        );
 }
 
 export default BookList
