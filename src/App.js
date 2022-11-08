13a873dda617b07c9c52aaabaeb2a024337e6bdc
stephen Munyao wambua <stephen.munyao@student.moringaschool.com>
2022-11-08 00:25:00 -0800

    Revert "added navbar routing"

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import BookDetails from './components/BookDetails';
 import Review from './components/Review';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import Books from './components/Books';
-import Contact from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import BookList from './components/BookList';
 
 function App() {
-  return( 
-  <Navbar />
-    
+  return (
+    <div className="App">
+    <BrowserRouter>
+    <Navbar/> 
+     <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route exact path='/BookList' element={<BookList />} />
+    </Routes>
+    </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,9 @@
-export default function Navbar(){
-    return <nav className="nav">
-        <a href="/" className="home"> Home </a>
-        <ul>
-            <li>
-                <a href="/Books">Books</a>
-            </li>
-            <li>
-                <a href="/Contact">Contact</a>
-            </li>
-        </ul>
-    </nav>
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
 }
+
+export default Navbar
