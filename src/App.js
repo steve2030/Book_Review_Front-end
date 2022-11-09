b92f9e3574e3fd0a293615e36e6baffc806280ce
Kazungu Derrick <kazunguderriq@gmail.com>
2022-11-09 11:11:55 +0300

    create footer

--- src/App.js
+++ src/App.js
@@ -6,26 +6,13 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+
 import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Footer />
     </div>
   );
 }
