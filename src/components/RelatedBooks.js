7d4ce9b6f690f544e82db8b97074d016b4ed9026
+ <+>
2022-11-10 08:18:33 +0300

    fetching books data

--- src/components/RelatedBooks.js
+++ src/components/RelatedBooks.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import data from '../data'
 import Card from './Card'
 
 export default function RelatedBooks() {
+
+    const baseApiUrl = "https://localhost:9292"
+    const [relatedBooks,setRelatedBooks] = useState([]);
+
+     useEffect(() => {
+        fetch(`${baseApiUrl}/book`)
+        .then((respone) => response.json())
+        .then((data) => console.log(data));
+     },[]);
+
     const cardData = data.map(item => {
         return (
             <Card
